f7053891e4e78e4f6b704b4bdc4ccec6dad294bd
Larsen <kylelars@physics.byu.edu>
2016-05-25 12:23:07 -0600

    Added everything to repo.

---
+++ js/main.js
@@ -0,0 +1,115 @@
+var him = [
+    /*{
+        src: "images/him_1.jpeg",
+        w: 1543,
+        h: 1118
+    },*/
+    {
+        src: "images/him_2.jpeg",
+        w: 1545,
+        h: 2162
+    },
+    {
+        src: "images/him_3.jpeg",
+        w: 1534,
+        h: 2165
+    },
+    {
+        src: "images/him_4.jpeg",
+        w: 1543,
+        h: 2175
+    },
+    {
+        src: "images/him_5.jpeg",
+        w: 1543,
+        h: 2175
+    },
+    {
+        src: "images/him_6.jpeg",
+        w: 1543,
+        h: 2175
+    },
+    {
+        src: "images/him_7.jpeg",
+        w: 1543,
+        h: 2175
+    },
+    {
+        src: "images/him_8.jpeg",
+        w: 1543,
+        h: 2175
+    },
+    {
+        src: "images/him_9.jpeg",
+        w: 1543,
+        h: 2175
+    }
+];
+
+var her = [
+    /*{
+        src: "images/her_1.jpeg",
+        w: 1543,
+        h: 1118
+    },*/
+    {
+        src: "images/her_2.jpeg",
+        w: 1543,
+        h: 2175
+    },
+    {
+        src: "images/her_3.jpeg",
+        w: 1543,
+        h: 2175
+    },
+    {
+        src: "images/her_4.jpeg",
+        w: 1543,
+        h: 2175
+    },
+    {
+        src: "images/her_5.jpeg",
+        w: 1543,
+        h: 2175
+    },
+    {
+        src: "images/her_6.jpeg",
+        w: 1543,
+        h: 2175
+    },
+    {
+        src: "images/her_7.jpeg",
+        w: 1543,
+        h: 2175
+    },
+    {
+        src: "images/her_8.jpeg",
+        w: 1543,
+        h: 2175
+    },
+    {
+        src: "images/her_9.jpeg",
+        w: 1543,
+        h: 2175
+    },
+    {
+        src: "images/her_10.jpeg",
+        w: 1543,
+        h: 2175
+    },
+    {
+        src: "images/her_11.jpeg",
+        w: 1543,
+        h: 2175
+    },
+    {
+        src: "images/her_12.jpeg",
+        w: 1543,
+        h: 2175
+    },
+    {
+        src: "images/her_13.jpeg",
+        w: 1543,
+        h: 2175
+    }
+];
